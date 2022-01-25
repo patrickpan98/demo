c63c4e42132c3084ecea44d82a0ab5fa25041dd4
IN-SO-002 <IN-SO-002@IN-SO-002>
2022-01-25 16:22:19 +0100

    Commit 2 : gestion des sessions.

--- web1/src/main/java/fr/formation/inti/controller/LoginServlet.java
+++ web1/src/main/java/fr/formation/inti/controller/LoginServlet.java
@@ -2,45 +2,89 @@ package fr.formation.inti.controller;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Servlet implementation class LoginServlet
  */
 public class LoginServlet extends HttpServlet {
+	private static final Log log = LogFactory.getLog(LoginServlet.class);
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
+	
+	
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Writer out = response.getWriter();
-		out.append("<html>")
-		.append("<head><title>Hello Servlet</title></head>")
-		.append("<body>")
-		.append("<h1>Hello LoginServlet!</h1>")
-		.append("</body>")
-		.append("</html>");
+		HttpSession session = request.getSession();
+		String msg = (String) session.getAttribute("message");
+		Date dateCon = (Date) session.getAttribute("dateConnection");
+		String userName = (String) session.getAttribute("userName");
+		
+		if (msg == null) {
+			response.sendRedirect(request.getContextPath());
+		}
+		else {
+			Writer out = response.getWriter();
+			out.append("<html>")
+			.append("<head><title>Login</title></head>")
+			.append("<body>")
+			.append("<p>Re-bonjour "+userName+"!<br>")
+			.append(msg+"<br>")
+			.append("Date : "+dateCon+"<br>")
+			.append("Session ID : "+session.getId()+"</p>")
+			.append("</body>")
+			.append("</html>");
+		}
+		
 	}
-
+	
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String user = request.getParameter("username");
+		String pass = request.getParameter("password");
+		HttpSession session = request.getSession();
+		
+		if ("root".equals(user) && "123456".equals(pass)) {
+			Writer out = response.getWriter();
+			Date date = new Date();
+			out.append("<html>")
+			.append("<head><title>Login</title></head>")
+			.append("<body>")
+			.append("<p>Hello "+user+", tu es connecté(e) !<br>")
+			.append("Date : "+date+"</p>")
+			.append("</body>")
+			.append("</html>");
+			
+			session.setAttribute("message", "You are connected!");
+			session.setAttribute("dateConnection", date);
+			session.setAttribute("userName", user);
+			
+		}
+		else {
+//			request.getRequestDispatcher("/").forward(request, response);
+			response.sendRedirect(request.getContextPath());
+		}
 	}
-
+	
 }
--- web1/src/main/java/fr/formation/inti/controller/SessionServlet.java
+++ web1/src/main/java/fr/formation/inti/controller/SessionServlet.java
@@ -6,18 +6,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class TestServlet
  */
-@WebServlet("/TestServlet")
-public class TestServlet extends HttpServlet {
+@WebServlet("/logout")
+public class SessionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public TestServlet() {
+    public SessionServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,8 +27,9 @@ public class TestServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		session.invalidate();
+		response.getWriter().append("You are disconnected!");
 	}
 
 	/**
